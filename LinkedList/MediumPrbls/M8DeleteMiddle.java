f387ba52b54a5347b0abb791a5e127f8ce266569
chakri8826 <kommuchakri8826@gmail.com>
2025-01-31 14:32:24 +0530

    7,8

---
+++ LinkedList/MediumPrbls/M8DeleteMiddle.java
@@ -0,0 +1,52 @@
+public class M8DeleteMiddle {
+    
+    public ListNode deleteMiddle(ListNode head) {
+        if(head==null || head.next==null) return null;
+        ListNode temp = head;
+        int n = 0;
+        while (temp != null) {
+            n++;
+            temp = temp.next;
+        }
+        int res = n / 2;
+        temp = head;
+        while (temp != null) {
+            res--;
+            if (res == 0) {
+                temp.next = temp.next.next;
+                break;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    // CHAKRI'S APPROACH (OPTIMAL)
+    // public ListNode deleteMiddle(ListNode head) {
+    //     if(head==null || head.next==null) return null;
+    //     ListNode slow=head,fast=head,slowPrev=null;
+    //     while(fast!=null && fast.next!=null){
+    //         slowPrev=slow;
+    //         slow=slow.next;
+    //         fast=fast.next.next;
+    //     }       
+    //     slowPrev.next=slow.next;
+    //     return head;
+    // }
+
+
+    // OPTIMAL
+    // public ListNode deleteMiddle(ListNode head) {
+    //     if(head==null || head.next==null) return null;
+    //     ListNode slow=head,fast=head;
+    //     fast=head.next.next;
+    //     while(fast!=null && fast.next!=null){
+    //         slow=slow.next;
+    //         fast=fast.next.next;
+    //     }       
+    //      if (slow.next != null) {
+    //         slow.next = slow.next.next;
+    //     }
+    //     return head;
+    // }
+}
