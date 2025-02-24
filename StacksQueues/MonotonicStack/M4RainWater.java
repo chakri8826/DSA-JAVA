7e87458eeda8bab9b212221f7554419520856936
chakri8826 <kommuchakri8826@gmail.com>
2025-02-24 17:51:08 +0530

    new

--- StacksQueues/MonotonicStack/M4RainWater.java
+++ StacksQueues/MonotonicStack/M4RainWater.java
@@ -30,33 +30,62 @@ public class M4RainWater {
 
 
 
+    // public int trap(int[] height) {
+    //     int n=height.length;
+    //     // int pre[] = new int[n];
+    //     int suff[] = new int[n];
+
+    //     // THIS CAN BE OPTIMIZED BY TRAVERSING IN LAST STEP ITSELF
+    //     // pre[0]=height[0];
+    //     // for(int i=1;i<n;i++){
+    //     //     pre[i]=Math.max(pre[i-1],height[i]);
+    //     // }
+
+    //     suff[n-1]=height[n-1];
+    //     for(int i=n-2;i>=0;i--){
+    //         suff[i]=Math.max(suff[i+1],height[i]);
+    //     }
+
+    //     int total=0;
+    //     int ml=Integer.MIN_VALUE;
+    //     for(int i=0;i<height.length;i++){
+    //         ml = Math.max(height[i],ml);
+    //         int mr = suff[i];
+    //         if(ml>height[i] && mr>height[i]){
+    //             total+=Math.min(ml,mr)-height[i];
+    //         }
+    //     }
+    //     return total;
+    // }
+
+
+
+
     public int trap(int[] height) {
-        int n=height.length;
-        // int pre[] = new int[n];
-        int suff[] = new int[n];
-
-        // THIS CAN BE OPTIMIZED BY TRAVERSING IN LAST STEP ITSELF
-        // pre[0]=height[0];
-        // for(int i=1;i<n;i++){
-        //     pre[i]=Math.max(pre[i-1],height[i]);
-        // }
-
-        suff[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            suff[i]=Math.max(suff[i+1],height[i]);
-        }
-
-        int total=0;
-        int ml=Integer.MIN_VALUE;
-        for(int i=0;i<height.length;i++){
-            ml = Math.max(height[i],ml);
-            int mr = suff[i];
-            if(ml>height[i] && mr>height[i]){
-                total+=Math.min(ml,mr)-height[i];
+        int n=height.length,l=0,r=n-1,lmax=0,rmax=0,total=0;
+        while(l<r){
+            if(height[l]<height[r]){
+                if(lmax>height[l]){
+                    total+=lmax-height[l];
+                }
+                else{
+                    lmax=height[l];
+                }
+                l++;
+            }
+            else{
+                if(rmax>height[r]){
+                    total+=rmax-height[r];
+                }
+                else{
+                    rmax=height[r];
+                }
+                r--;
             }
         }
         return total;
     }
+
 
 
 
