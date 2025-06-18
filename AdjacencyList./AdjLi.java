4f00bc777f7f87dffa8e6f5cc220dd91d57a1e2b
KOMMU CHAKRADHAR <kommuchakri8826@gmail.com>
2025-06-18 09:53:57 +0530

    Delete AdjacencyList. directory

--- AdjacencyList./AdjLi.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class AdjLi {
-    public static void main(String[] args) {
-        int v=5;
-        ArrayList<ArrayList<Integer>> al = new ArrayList<ArrayList<Integer>>(v);
-        al.add(new ArrayList<Integer>());
-        al.get(0);
-    }
-}
