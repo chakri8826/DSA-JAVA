ed6f3fa94a1f0b6732b5c31341186b12a21e79e3
chakri8826 <kommuchakri8826@gmail.com>
2026-01-02 23:16:23 +0530

    updated clone linkedlist problem

--- LinkedList/HardLL/H4CloneLL.java
+++ LinkedList/HardLL/H4CloneLL.java
@@ -16,36 +16,50 @@ public class H4CloneLL {
     //     return map.get(head);
     // }
 
-    public Node copyRandomList(Node head) {    
-        if(head==null) return null;
-        Node temp=head;
-        while(temp!=null){
-            Node res = new Node(temp.val);
-            res.next=temp.next;
-            temp.next=res;
-            temp=temp.next.next;
-        }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        Node temp = head;
+        while(temp!=null){
+            Node cNode = new Node(temp.val);
+            cNode.next = temp.next;
+            temp.next = cNode;
+            temp = temp.next.next;
+        }
         temp=head;
         while(temp!=null){
             if(temp.random!=null){
-                temp.next.random=temp.random.next;
+                temp.next.random = temp.random.next;
             }
-            temp=temp.next.next;
+            temp = temp.next.next;
         }
-
-        Node pre=null;
-        temp=head;
-        Node newHead=temp.next;
+        Node dummy = new Node(-1);
+        Node res = dummy;
+        temp = head;
         while(temp!=null){
-            pre=temp;
-            temp=temp.next.next;
-            if(temp!=null){
-                pre.next.next=temp.next;
-            }
-            pre.next=temp;
+            res.next = temp.next;
+            temp.next = temp.next.next;
+            res = res.next;
+            temp = temp.next;
         }
-        return newHead;
-
+        return dummy.next;
     }
 }
