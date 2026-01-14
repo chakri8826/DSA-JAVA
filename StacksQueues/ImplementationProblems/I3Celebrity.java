5e8b371c6e1eff2bf285f7919419f15036ee7ab6
chakri8826 <kommuchakri8826@gmail.com>
2026-01-14 20:01:33 +0530

    Updated Celebrity problem

--- StacksQueues/ImplementationProblems/I3Celebrity.java
+++ StacksQueues/ImplementationProblems/I3Celebrity.java
@@ -1,4 +1,35 @@
 public class I3Celebrity {
+    
+    // public int celebrity(int mat[][]) {
+    //     int n = mat.length;
+
+    //     // Arrays to store knowledge information
+    //     int[] iknow = new int[n];  // Counts how many people `i` knows
+    //     int[] known = new int[n];  // Counts how many people know `i`
+    
+    //     // Fill the arrays based on the matrix
+    //     for (int i = 0; i < n; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             if (i!=j && mat[i][j] == 1) {
+    //                 iknow[i]++;   // `i` knows `j`
+    //                 known[j]++;   // `j` is known by `i`
+    //             }
+    //         }
+    //     }
+    
+    //     // Find the potential celebrity
+    //     for (int i = 0; i < n; i++) {
+    //         if (iknow[i] == 0 && known[i] == n - 1) {
+    //             return i; // Found the celebrity
+    //         }
+    //     }
+        
+    //     return -1; // No celebrity found
+    // }
+
+
+    
+
      // TC -> O(N^2)+O(N^2),SC=O(1)
     // public int celebrity(int mat[][]) {
     //     int r = mat.length,c=mat[0].length;
@@ -26,38 +57,13 @@ public class I3Celebrity {
     
     
     
-    // public int celebrity(int mat[][]) {
-    //     int n = mat.length;
-
-    //     // Arrays to store knowledge information
-    //     int[] iknow = new int[n];  // Counts how many people `i` knows
-    //     int[] known = new int[n];  // Counts how many people know `i`
-
-    //     // Fill the arrays based on the matrix
-    //     for (int i = 0; i < n; i++) {
-    //         for (int j = 0; j < n; j++) {
-    //             if (i!=j && mat[i][j] == 1) {
-    //                 iknow[i]++;   // `i` knows `j`
-    //                 known[j]++;   // `j` is known by `i`
-    //             }
-    //         }
-    //     }
-
-    //     // Find the potential celebrity
-    //     for (int i = 0; i < n; i++) {
-    //         if (iknow[i] == 0 && known[i] == n - 1) {
-    //             return i; // Found the celebrity
-    //         }
-    //     }
-        
-    //     return -1; // No celebrity found
-    // }
     
     
     
     
     public int celebrity(int mat[][]) {
         int n=mat.length,t=0,b=n-1;
+        // When i == j → loop stops
         while(t<b){
             if(mat[t][b]==1)t++;
             else if(mat[b][t]==1)b--;
